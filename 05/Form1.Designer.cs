117d0b78dfe0193c454f1d4674ffdb8d38288de7
Din0 <100676664+su-din0@users.noreply.github.com>
2024-01-29 21:19:48 +0100

    Příklad 5

---
+++ 05/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace _05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(732, 106);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 05/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+        Vytvořte program, který bude ze seznamu obrázků náhodně vybírat jeden a zobrazovat v komponentě PictureBox.
+        Při kliku na vybraný obrázek se obrázek smaže ze seznamu i z komponenty PictureBox.
+        Obrázky si ulož do komponenty ImageLis. Bitmapy obrázků ulož do Listu.
+        A dále již pracuj s Listem.
+        */
+        private List<Bitmap> list;
+        private Random rnd;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int index = rnd.Next(0, list.Count);
+            if (list.Count > 0)
+            {
+                pictureBox1.Image = list[index];
+                list.RemoveAt(index);
+            }
+            else
+            {
+                pictureBox1.Image = null;
+                MessageBox.Show("Žádný obrázek není k dispozici");
+            }
+                
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rnd = new Random();
+            list = new List<Bitmap>();
+
+            list.Add(new Bitmap("images\\1.png"));
+            list.Add(new Bitmap("images\\2.jpg"));
+            list.Add(new Bitmap("images\\3.png"));
+            list.Add(new Bitmap("images\\4.png"));
+            list.Add(new Bitmap("images\\5.png"));
+
+            int index = rnd.Next(0, list.Count);
+            pictureBox1.Image = list[index];
+
+        }
+    }
+}
